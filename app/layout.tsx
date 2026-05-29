45486ed1cfb71703eb4ea47a0d8c70add278172d
Geovanni Aguilar <geovannia@MacBookAir.home.local>
2026-05-29 12:57:35 -0700

    Added Analytics to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -82,6 +83,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
           </div>
         </footer>
+
+        <Analytics />
       </body>
     </html>
   );
